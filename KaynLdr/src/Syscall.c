bdeb8cc820600b85114b4c23aa692767fc2cab0e
Cracked5pider <cracked5pider@protonmail.com>
2021-12-28 19:03:38 +0100

    replaced __builtin_memcpy with __movsb

--- KaynLdr/src/Syscall.c
+++ KaynLdr/src/Syscall.c
@@ -28,7 +28,7 @@ WORD GetSyscall( PVOID pModuleBase, PIMAGE_EXPORT_DIRECTORY pImageExportDirector
                 && *((PBYTE)pFunctionAddress + 6) == 0x00
                 && *((PBYTE)pFunctionAddress + 7) == 0x00)
             {
-                __builtin_memcpy(&wSystemCall, (pFunctionAddress + 4), 2);
+                __movsb(&wSystemCall, (pFunctionAddress + 4), 2);
                 return wSystemCall;
             }
 
@@ -43,7 +43,7 @@ WORD GetSyscall( PVOID pModuleBase, PIMAGE_EXPORT_DIRECTORY pImageExportDirector
                         && *((PBYTE)pFunctionAddress + 6 + idx * DOWN) == 0x00
                         && *((PBYTE)pFunctionAddress + 7 + idx * DOWN) == 0x00)
                     {
-                        __builtin_memcpy(&wSystemCall, (pFunctionAddress + 4 + idx * DOWN), 2);
+                        __movsb(&wSystemCall, (pFunctionAddress + 4 + idx * DOWN), 2);
                         return wSystemCall;
 
                     }
@@ -55,7 +55,7 @@ WORD GetSyscall( PVOID pModuleBase, PIMAGE_EXPORT_DIRECTORY pImageExportDirector
                         && *((PBYTE)pFunctionAddress + 6 + idx * UP) == 0x00
                         && *((PBYTE)pFunctionAddress + 7 + idx * UP) == 0x00)
                     {
-                        __builtin_memcpy(&wSystemCall, (pFunctionAddress + 4 + idx * UP), 2);
+                        __movsb(&wSystemCall, (pFunctionAddress + 4 + idx * UP), 2);
                         return wSystemCall;
                     }
 
@@ -73,7 +73,7 @@ WORD GetSyscall( PVOID pModuleBase, PIMAGE_EXPORT_DIRECTORY pImageExportDirector
                         && *((PBYTE)pFunctionAddress + 6 + idx * DOWN) == 0x00
                         && *((PBYTE)pFunctionAddress + 7 + idx * DOWN) == 0x00)
                     {
-                        __builtin_memcpy(&wSystemCall, (pFunctionAddress + 4 + idx * DOWN), 2);
+                        __movsb(&wSystemCall, (pFunctionAddress + 4 + idx * DOWN), 2);
                         return wSystemCall;
                     }
 
@@ -84,7 +84,7 @@ WORD GetSyscall( PVOID pModuleBase, PIMAGE_EXPORT_DIRECTORY pImageExportDirector
                         && *((PBYTE)pFunctionAddress + 6 + idx * UP) == 0x00
                         && *((PBYTE)pFunctionAddress + 7 + idx * UP) == 0x00)
                     {
-                        __builtin_memcpy(&wSystemCall, (pFunctionAddress + 4 + idx * UP), 2);
+                        __movsb(&wSystemCall, (pFunctionAddress + 4 + idx * UP), 2);
                         return wSystemCall;
                     }
 
